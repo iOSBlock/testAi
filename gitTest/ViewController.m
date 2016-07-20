2168bd675ab1dc83ad67c7a94c7ef6679818f3b0
Slaser <Slaser@SlaserdeMac-mini.local>
2016-07-20 18:00:56 +0800

    sourceTree test

--- gitTest/ViewController.m
+++ gitTest/ViewController.m
@@ -27,4 +27,15 @@
 {
     NSLog(@"提交Git");
 }
+-(void)testSourceTree
+{
+    NSLog(@"提交SourceTree");
+    NSLog(@"我再打印一下,重新提交");
+}
+-(void)OneMoreTime
+{
+    int i = 1;
+    i  = i + 1;
+}
+
 @end
